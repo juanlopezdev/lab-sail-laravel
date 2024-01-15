7269e26b6c2af39b2843da3aff21fa25875bbafa
Juan Lopez <jlopezj@proveedor.intercorp.com.pe>
2024-01-15 11:59:09 -0500

    Base de datos relacional con Eloquent

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -28,14 +28,11 @@ class ChirpController extends Controller
      */
     public function store(Request $request)
     {
-        $request->validate([
+        $validated = $request->validate([
             'message' => ['required', 'min:3', 'max:255'], // More validations in laravel.com/docs/validation
         ]);
 
-        Chirp::create([
-            'message' => $request->get('message'),
-            'user_id' => auth()->id(),
-        ]);
+        $request->user()->chirps()->create($validated);
 
         return to_route('chirps.index')
             ->with('status', __('Chirp created!'));
